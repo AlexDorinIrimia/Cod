1e5dd95418d0d89b981ecf4b80ec491c680c9153
Irimia Dorin-Alexandru <alexdorinirimia06@yahoo.com>
2023-03-21 16:55:14 +0200

    ceva cod ptyhon

---
+++ task.py
@@ -0,0 +1,14 @@
+class task:
+    def __init__(self,letter_idx, word_idx,letter):
+        self.letter_idx = letter_idx
+        self.word_idx = word_idx
+        self.letter = letter
+    
+    #def arrange_by_word_idx(self):
+        
+array = [
+    file = open("/home/alex/.cbsl_test/JasonShards/shards.json","r")
+    i = 0
+    while file.read():
+        array[i] = task()
+]
